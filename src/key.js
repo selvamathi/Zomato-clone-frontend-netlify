885e37b14ad687db74113776c1bdc051b1b936f3
selvamathi13 <selvamathi2001@gmail.com>
2021-11-07 16:27:01 +0530

    url changed

--- src/key.js
+++ src/key.js
@@ -1,7 +1,4 @@
-export const API_URL = window.location.hostname.includes("localhost")
-  ? "https://zomato-clone-selvamathi.herokuapp.com"
-  : "/node";
+export const API_URL = "https://zomato-clone-selvamathi.herokuapp.com";
 
-export const CLIENT_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:3000"
-  : "/";
+export const CLIENT_URL = "https://zomato-clone-selvamathi.netlify.app/";
+
